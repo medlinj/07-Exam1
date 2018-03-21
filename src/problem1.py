190f3dab9fa0d56f82822cef03c25b96af3792f1
medlinj <medlinj@rose-hulman.edu>
2018-03-20 23:01:33 -0400

    Exam 1 Final Push

--- src/problem1.py
+++ src/problem1.py
@@ -3,7 +3,7 @@ Exam 1, problem 1.
 
 Authors: David Mutchler, Dave Fisher, Matt Boutell, Amanda Stouder,
          their colleagues and JD Medlin.  March 2018.
-"""  # DONE: 1. PUT YOUR NAME IN THE ABOVE LINE.  
+"""  # DONE: 1. PUT YOUR NAME IN THE ABOVE LINE.     
 
 
 def main():
