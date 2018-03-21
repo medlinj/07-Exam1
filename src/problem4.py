5420a8960972010755cf1445642b3fe21cc7c82c
medlinj <medlinj@rose-hulman.edu>
2018-03-20 22:46:53 -0400

    Exam 1

--- src/problem4.py
+++ src/problem4.py
@@ -2,8 +2,8 @@
 Exam 1, problem 4.
 
 Authors: David Mutchler, Dave Fisher, Matt Boutell, Amanda Stouder,
-         their colleagues and PUT_YOUR_NAME_HERE.  March 2018.
-"""  # TODO: 1. PUT YOUR NAME IN THE ABOVE LINE.
+         their colleagues and JD Medlin.  March 2018.
+"""  # DONE: 1. PUT YOUR NAME IN THE ABOVE LINE.
 
 
 def main():
@@ -138,6 +138,11 @@ def problem4(m):
     # TODO: 7. Implement and test this function.
     #          Tests have been written for you (above).
     # -------------------------------------------------------------------------
+    count = 0
+    for k in range(m):
+        if m % (k + 1) == 0:
+            count = count + 1
+    return count - 2
 
 
 # -----------------------------------------------------------------------------
